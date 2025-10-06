235cb79fa665f5057332ee54c7c56b3bd5a63fb2
B. <bd90962@gmail.com>
2025-10-06 19:17:21 +0200

    Pendiente Ejercicio 10 de la relacion3

--- relacion3/10piramida_numeros_impares.py
+++ relacion3/10piramida_numeros_impares.py
@@ -1,2 +1,21 @@
 
-''''''
+'''Escribe un programa que recoja un número y muestre un triángulo formado por
+secuencias decrecientes de números impares. Por ejemplo, si se introduce el
+5 se debe mostrar:
+1
+3 1
+5 3 1
+7 5 3 1
+9 7 5 3 1'''
+
+n = int(input("Introduzca un número para la base del triángulo: "))
+
+for i in range(1, n + 1):
+
+    primeroFila = (2 * i) - 1 # determinar el numero de comienzo de cada fila
+
+    # por cada fila se comienza imprimiendo desde el primer numero de fila (primeroFila)
+    # terminando en 1, decreciendo en 2 (para imprimir solo impares)
+    for j in range(1, primeroFila, 2):
+
+        print (str(j))
