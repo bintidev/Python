497f6e7c8d405bf4dffea8f8685073fba16898da
B. <bd90962@gmail.com>
2025-11-02 19:47:11 +0100

    Cambios Minimos relacion5

--- relacion5/comprobacion.py
+++ relacion5/comprobacion.py
@@ -5,12 +5,8 @@
 def distincionMinMay(txt1, txt2, r):
 
     if (r.lower() == "n"):
-<<<<<<< HEAD
-        return [txt1.lower(), txt2.lower()]
-=======
         # elimina los espacios
         return [txt1.lower().replace(" ", ""), txt2.lower().replace(" ", "")]
->>>>>>> 3446999be6c737c4cd6c265a0155b94e3e884197
     
     else:
         return [txt1.replace(" ", ""), txt2.replace(" ", "")]
