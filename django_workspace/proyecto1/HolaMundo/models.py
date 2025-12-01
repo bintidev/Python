ae83ed6ae82a250bb580222f8b8200dc1f92a462
B. <bd90962@gmail.com>
2025-12-01 20:58:38 +0100

    Crecion de Nueva Tabla Modificando Fichero Models de App HolaMundo

--- django_workspace/proyecto1/HolaMundo/models.py
+++ django_workspace/proyecto1/HolaMundo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+# Create your models here.
+# Al aplicar esta herencia, Django va a saber que Author es una tabla en la BD
+class Author (models.Model):
+
+    name = models.CharField (
+        verbose_name ='Nombre', # etiqueta dentro de la tabla
+        max_length = 100,
+        default = ''
+    )
+
+    last_name = models.CharField(
+        verbose_name='Apellido',
+        max_length = 150,
+        default = ''
+    )
+
+    mail = models.CharField(
+        verbose_name ='Email',
+        max_length = 150,
+        default = ''
+    )
